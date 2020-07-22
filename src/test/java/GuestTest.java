d101dd63d2878cdce98facd5d66d4f57bf011fca
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-07-22 10:38:31 +0100

    Add getter method for guest name

--- src/test/java/GuestTest.java
+++ src/test/java/GuestTest.java
@@ -1,2 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuestTest {
+    Guest guest1;
+
+    @Before
+    public void setup() {
+        guest1 = new Guest("Harrison");
+    }
+
+    @Test
+    public void testGuestConstructor() {
+        assertEquals("Harrison", guest1.getName());
+    }
 }
