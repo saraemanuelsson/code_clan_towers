bdba5822beadef922509b14f2a4d6e2e8ae523a5
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-07-22 10:43:31 +0100

    Add Room class with getter methods for name and capacity

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+
 public class Room {
+
+    private String name;
+    private int capacity;
+    private ArrayList<Guest> guests;
+
+    public Room(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+        this.guests = new ArrayList<Guest>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
 }
--- src/test/java/RoomTest.java
+++ src/test/java/RoomTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class RoomTest {
+    Room room1;
+
+    @Before
+    public void setup() {
+        room1 = new Room("1", 2);
+    }
+
+    @Test
+    public void testRoomConstructor() {
+        assertEquals("1", room1.getName());
+        assertEquals(2, room1.getCapacity());
+    }
 }
